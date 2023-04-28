d811e80e8d405889aeef5095c7a170d0f805132f
Takuya Yonezawa <yonezawa.takuya.yq2@gmail.com>
2023-04-29 02:43:23 +0900

    up

--- lib/cdk-ts-stack.ts
+++ lib/cdk-ts-stack.ts
@@ -1,6 +1,9 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Runtime } from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { SqsEventSource } from 'aws-cdk-lib/aws-lambda-event-sources';
 
 export class CdkTsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -9,8 +12,15 @@ export class CdkTsStack extends cdk.Stack {
     // The code that defines your stack goes here
 
     // example resource
-    // const queue = new sqs.Queue(this, 'CdkTsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+    const queue = new sqs.Queue(this, 'CdkTsQueue', {
+      visibilityTimeout: cdk.Duration.seconds(300)
+    });
+    const eventSource = new SqsEventSource(queue);
+
+    const func = new NodejsFunction(this, "Function", {
+        entry: "lib/lambda/hello_world.ts",
+        runtime: Runtime.NODEJS_18_X,
+      });
+      func.addEventSource(eventSource);
+    }
 }
