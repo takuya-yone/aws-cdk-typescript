614698c956e46eeb7000d5825ba4a931fb76bede
Takuya Yonezawa <yonezawa.takuya.yq2@is.naist.jp>
2023-04-30 03:58:33 +0900

    up

--- lib/cdk-ts-stack.ts
+++ lib/cdk-ts-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import { Runtime } from 'aws-cdk-lib/aws-lambda';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { SqsEventSource } from 'aws-cdk-lib/aws-lambda-event-sources';
@@ -9,7 +10,13 @@ export class CdkTsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const table = new dynamodb.Table(this, 'Table', {
+      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+    });
+
+    // console.log(table);
+    // console.log("heeelleoeoeieoeoe--e-e--");
 
     // example resource
     const queue = new sqs.Queue(this, 'CdkTsQueue', {
@@ -20,6 +27,7 @@ export class CdkTsStack extends cdk.Stack {
     const func = new NodejsFunction(this, 'Function', {
       entry: 'lib/lambda/hello_world.ts',
       runtime: Runtime.NODEJS_18_X,
+      environment: { DYNAMO_TABLE_NAME: table.tableName },
     });
     func.addEventSource(eventSource);
   }
